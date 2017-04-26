db67f22e39e71f7287b31637bd9cf3ad84197aa4
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-26 15:51:35 -0400

    Yachtski method

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :episode
   belongs_to :artist
+
+  def yachtski
+    (self.dave_score + self.jd_score + self.hunter_score + self.steve_score) / 4.0
+  end
 end
