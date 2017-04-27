fc9f0c883861323a0f05855bb8072162a2f4bde0
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-27 16:35:32 -0400

    Abs deviations by song

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,4 +5,13 @@ class Song < ActiveRecord::Base
   def yachtski
     (self.dave_score + self.jd_score + self.hunter_score + self.steve_score) / 4.0
   end
+
+  def host_deviations
+    devs = {
+      jd_deviation: (self.yachtski - self.jd_score).abs,
+      hunter_deviation: (self.yachtski - self.hunter_score).abs,
+      steve_deviation: (self.yachtski - self.steve_score).abs,
+      dave_deviation: (self.yachtski - self.dave_score).abs
+    }
+  end
 end
