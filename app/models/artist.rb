6c1aaee6bb9828fc6bc282e6525482579caad7fa
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-27 13:04:07 -0400

    Round total

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,6 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def total_avg
-    (self.songs.inject(0) {|sum, song| sum + song.yachtski}) / self.songs.length.to_f
+    ((self.songs.inject(0) {|sum, song| sum + song.yachtski}) / self.songs.length.to_f).round(2)
   end
 end
