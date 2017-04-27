6a0fcfa5989d957f1cdc9b3814894f7451cf5034
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-27 10:04:14 -0400

    Remove unneeded gems

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,6 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
-require 'react-sinatra'
-require 'execjs'
-require 'mini_racer'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
