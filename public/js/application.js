e7648fec5b438453bbffb21737ce68cb96ef1299
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-27 09:42:18 -0400

    Case insensitive search working

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 
+  jQuery.expr[':'].Contains = function(a,i,m){
+    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+  };
+
   $('#songSearch').keyup(function() {
-    console.log('ok');
+    var query = $(this).val();
+
+    $('.song-list').find('.title:Contains(' + query + ')').parent().show()
+    $('.song-list').find('.title:not(:Contains(' + query + '))').parent().hide()
   });
 
 });
