a1d719267f65d2d691b32d0534fd75034c294ba3
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-28 08:59:13 -0400

    Ugly avg deviation method

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,10 +8,32 @@ class Song < ActiveRecord::Base
 
   def host_deviations
     devs = {
-      jd_deviation: (self.yachtski - self.jd_score).abs,
-      hunter_deviation: (self.yachtski - self.hunter_score).abs,
-      steve_deviation: (self.yachtski - self.steve_score).abs,
-      dave_deviation: (self.yachtski - self.dave_score).abs
+      jd: (self.yachtski - self.jd_score).abs,
+      hunter: (self.yachtski - self.hunter_score).abs,
+      steve: (self.yachtski - self.steve_score).abs,
+      dave: (self.yachtski - self.dave_score).abs
     }
   end
+
+  # refactor this
+  def self.avg_host_deviations
+    total_songs = Song.all.length
+    total_dev = self.all.inject({jd: 0, hunter: 0, steve: 0, dave: 0}) do |memo, song|
+      devs = song.host_deviations
+      memo[:jd] += devs[:jd]
+      memo[:hunter] += devs[:hunter]
+      memo[:steve] += devs[:steve]
+      memo[:dave] += devs[:dave]
+      memo
+    end
+
+    avg_dev = {
+      jd: (total_dev[:jd] / total_songs),
+      hunter: (total_dev[:hunter] / total_songs),
+      steve: (total_dev[:steve] / total_songs),
+      dave: (total_dev[:dave] / total_songs),
+    }
+    avg_dev
+  end
+
 end
