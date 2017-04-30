f3a1c9acffbd36b1cd285319e5536461e85e26a0
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-30 13:43:59 -0400

    Taking average from yachtski form

--- public/js/application.js
+++ public/js/application.js
@@ -11,4 +11,21 @@ $(document).ready(function() {
     $('.song-list').find('.title:not(:Contains(' + query + '))').parent().hide()
   });
 
+  $('.score-input').keyup(function() {
+    var sum = 0;
+    var avg = 0;
+    $('.score-input').each(function() {
+      sum += Number($(this).val());
+    });
+
+    var $allInput = $('.score-input');
+    var $emptyInput = $('.score-input').filter(function(){return this.value==''});
+    var full = $allInput.length - $emptyInput.length;
+
+    if (full > 0) {avg = (sum/full)}
+
+    $('.yachtski-score').text(avg)
+
+  });
+
 });
