9bc035fa1b76dd1ccc249a676e1d6da3d8873e3d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-01 09:57:27 -0400

    Create route for episode

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,3 +2,13 @@ get '/episodes/new' do
   @recent_episode = Episode.all.last
   erb :'episodes/new'
 end
+
+post '/episodes' do
+  @episode = Episode.new(number: params[:episode][:number])
+  if @episode.save
+    redirect to "/episodes/#{@episode.id}"
+  else
+    @recent_episode = Episode.all.last
+    erb :'episodes/new'
+  end
+end
