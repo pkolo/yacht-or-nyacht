f275a8a23c3f7da417ae240b61c0f900027ce5a2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-01 14:47:31 -0400

    Create route for new song

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -27,3 +27,21 @@ get '/episodes/:id/songs/new' do
   @episode = Episode.find(params[:id])
   erb :'/songs/_form', layout: false
 end
+
+post '/episodes/:id/songs' do
+  data = params[:song]
+  @episode = Episode.find(params[:id])
+  @artist = Artist.find_or_create_by(name: data[:artist])
+
+  if @artist
+    @song = Song.new(title: data[:title], year: data[:year], jd_score: data[:jd_score], hunter_score: data[:hunter_score], steve_score: data[:steve_score], dave_score: [:dave_score])
+    @song.artist = @artist
+    @song.episode = @episode
+
+    if @song.save
+      erb :'/songs/_list_item', layout: false, locals: {song: @song}
+    else
+      "error"
+    end
+  end
+end
