5773543cb4ceccf4e87e2eaedd34355ed398463a
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-02 14:51:04 -0400

    Revert to 7a4d451

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,11 +3,6 @@ get '/' do
   erb :'songs/index'
 end
 
-get '/stats' do
-  @hosts = ["jd", "hunter", "steve", "dave"]
-  erb :'songs/stats'
-end
-
 get '/songs/:id' do
   @song = Song.find(params[:id])
   erb :'songs/show'
@@ -20,12 +15,11 @@ end
 
 get '/songs/:id/discog_search' do
   @song = Song.find(params[:id])
-  @response = @song.discog_search
-  @results = @response["results"].sort_by {|result| result["community"]["have"]}.reverse.first(10)
-  binding.pry
+  @results = @song.discog_search["results"].sort_by {|result| result["community"]["have"]}.reverse.first(10)
   erb :'songs/_search_results', layout: false
 end
 
-# post '/songs/:id/add_personnel' do
-#   render "test"
-# end
+get '/stats' do
+  @hosts = ["jd", "hunter", "steve", "dave"]
+  erb :'songs/stats'
+end
