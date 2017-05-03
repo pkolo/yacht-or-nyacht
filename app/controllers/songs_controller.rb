f7bcf5d76471bf6416e551ace6e694b96540d42d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-03 17:03:38 -0400

    Automatically adds album if already in DB

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,13 @@ end
 get '/songs/:id/discog_search' do
   @song = Song.find(params[:id])
   @results = @song.discog_search["results"].sort_by {|result| result["community"]["have"]}.reverse.first(10)
-  erb :'songs/_search_results', layout: false
+  if Album.match_in(@results)
+    url = "https://api.discogs.com/releases/" + Album.match_in(@results)
+    @credits = @song.add_personnel(url)
+    erb :'songs/_personnel', layout: false, locals: {credits: @credits}
+  else
+    erb :'songs/_search_results', layout: false
+  end
 end
 
 post '/songs/:id/add_personnel' do
--- app/models/album.rb
+++ app/models/album.rb
@@ -7,4 +7,9 @@ class Album < ActiveRecord::Base
     total_pts = self.songs.inject(0) { |sum, song| sum += song.yachtski }
     (total_pts / self.songs.length)
   end
+
+  def self.match_in(results)
+    result_ids = results.map {|result| result["id"].to_s}
+    self.where(discog_id: result_ids).first.discog_id
+  end
 end
