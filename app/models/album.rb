5206ddbc910700e83822c8a79b5b62dbe794544c
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-03 16:44:24 -0400

    Average album yachtski

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,9 @@ class Album < ActiveRecord::Base
   belongs_to :artist
   has_many :songs
   has_many :credits, as: :creditable
+
+  def avg_yachtski
+    total_pts = self.songs.inject(0) { |sum, song| sum += song.yachtski }
+    (total_pts / self.songs.length)
+  end
 end
