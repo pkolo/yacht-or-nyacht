639b0c47dd99d188f2bcb3b8e14323cd8e0b7a45
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-05 13:20:59 -0400

    Jay Gradient

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,39 @@
 $(document).ready(function() {
 
+  var jayGradient = function(pct) {
+    var percentColors = [
+    { pct: 0.0, color: { r: 230, g: 124, b: 115 } },
+    { pct: 0.5, color: { r: 255, g: 214, b: 102 } },
+    { pct: 1.0, color: { r: 87, g: 187, b: 138 } } ];
+
+    for (var i = 1; i < percentColors.length - 1; i++) {
+    if (pct < percentColors[i].pct) {
+        break;
+      }
+    }
+
+    var lower = percentColors[i - 1];
+    var upper = percentColors[i];
+    var range = upper.pct - lower.pct;
+    var rangePct = (pct - lower.pct) / range;
+    var pctLower = 1 - rangePct;
+    var pctUpper = rangePct;
+    var color = {
+        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+    };
+    return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+    // or output as hex if preferred
+  };
+
   $('.yachtski').each(function() {
     var $this = $(this);
     var yachtski = $this.text()
+    var color = jayGradient(yachtski / 100)
 
-    if (yachtski >= 50 ) {
-      $this.css('background-color', '#afa')
-    }
+    $this.css('background-color', color)
+    $this.parent().css('border', `1px solid ${color}`)
   });
 
   jQuery.expr[':'].Contains = function(a,i,m){
