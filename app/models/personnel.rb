5e3feaa961012c7bff12cf1ff87e8e97d3a3783d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-06 13:23:24 -0400

    Non-performer cred assoc for personnel

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -5,4 +5,7 @@ class Personnel < ActiveRecord::Base
 
   has_many :song_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Song'
   has_many :album_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Album'
+
+  has_many :song_credits, ->(credit) { where 'credits.role != ? AND credits.creditable_type = ?', "Artist", "Song" }, class_name: 'Credit'
+  has_many :album_credits, ->(credit) { where 'credits.role != ? AND credits.creditable_type = ?', "Artist", "Album" }, class_name: 'Credit'
 end
