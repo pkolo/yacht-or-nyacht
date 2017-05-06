ffb1bacaaa9806f1d2c0fef7c10d66848fa27503
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-06 13:06:22 -0400

    Associations for creditable by

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,3 +1,8 @@
 class Personnel < ActiveRecord::Base
   has_many :credits
+  has_many :songs, through: :credits, source: :creditable, source_type: 'Song'
+  has_many :albums, through: :credits, source: :creditable, source_type: 'Album'
+
+  has_many :song_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Song'
+  has_many :album_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Album'
 end
