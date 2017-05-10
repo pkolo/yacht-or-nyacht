47137c381dcb7d1cf2de8bae34d93fc94d3f256e
Patrick Kolodgy <pkolodgy@gmail.com>
2017-05-10 09:35:29 -0400

    Improve combined credit methods

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -10,29 +10,25 @@ class Personnel < ActiveRecord::Base
   has_many :album_credits, ->(credit) { where 'credits.role != ? AND credits.creditable_type = ?', "Artist", "Album" }, class_name: 'Credit'
 
   def combined_song_credits
-    self.song_credits.each_with_object({}) do |credit, memo|
-      if memo[credit.creditable.title]
-        memo[credit.creditable.title][:roles] << credit.role
-      else
-        memo[credit.creditable.title] = {
-          id: credit.creditable.id,
-          roles: [credit.role]
-        }
-      end
+    combined_credits = self.song_credits.uniq.each_with_object([]) do |credit, memo|
+      combined_credit = {
+        media: credit.creditable,
+        roles: self.song_credits.where(creditable_id: credit.creditable.id).pluck(:role)
+      }
+      memo << combined_credit
     end
+    combined_credits.uniq
   end
 
   def combined_album_credits
-    self.album_credits.each_with_object({}) do |credit, memo|
-      if memo[credit.creditable.title]
-        memo[credit.creditable.title][:roles] << credit.role
-      else
-        memo[credit.creditable.title] = {
-          id: credit.creditable.id,
-          roles: [credit.role]
-        }
-      end
+    combined_credits = self.album_credits.each_with_object([]) do |credit, memo|
+      combined_credit = {
+        media: credit.creditable,
+        roles: self.album_credits.where(creditable_id: credit.creditable.id).pluck(:role)
+      }
+      memo << combined_credit
     end
+    combined_credits.uniq
   end
 
 end
