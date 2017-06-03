35e75c3a880a81483e1b5e550e6a8a6a568fbd35
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-03 16:52:08 -0400

    Fix personnel page and partials

--- app/controllers/personnel_controller.rb
+++ app/controllers/personnel_controller.rb
@@ -1,5 +1,6 @@
 get '/personnel/:id' do
   @personnel = Personnel.find(params[:id])
+  @yachtski = @personnel.yachtski
   @song_performances = @personnel.song_performances.sort_by {|song| song.yachtski }.reverse
   @song_credits = @personnel.combined_song_credits.sort_by { |credit| credit[:yachtski] }.reverse
   @album_credits = @personnel.combined_album_credits.sort_by { |credit| credit[:yachtski] }.reverse
