ec89ac08b79f6e5d9ffc462ee8d382b92aebc977
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-04 15:03:06 -0400

    Update seed for new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-uri = URI.parse("https://spreadsheets.google.com/feeds/list/1xQbknvF7FuTJcsVBLgm1KlkozUhVVpJiyVOjvtSol0E/od6/public/values?alt=json")
+uri = URI.parse("https://spreadsheets.google.com/feeds/list/182FzE4GNjjyw8RN9R7fLIOl5GIV-ax00JNuxgdhaUgA/od6/public/values?alt=json")
 
 response = Net::HTTP.get_response(uri)
 
@@ -13,6 +13,6 @@ json_res["feed"]["entry"].each do |entry|
   credit = Credit.new(personnel: artist, role: "Artist")
   episode = Episode.find_or_create_by(number: entry["gsx$show"]["$t"])
 
-  song = Song.create(title: entry["gsx$title"]["$t"], year: entry["gsx$year"]["$t"].to_i, jd_score: entry["gsx$jdryznar"]["$t"].to_f, hunter_score: entry["gsx$hunterstair"]["$t"].to_f, dave_score: entry["gsx$davelyons"]["$t"].to_f, steve_score: entry["gsx$stevehuey"]["$t"].to_f, episode: episode)
+  song = Song.create(title: entry["gsx$title"]["$t"], year: entry["gsx$year"]["$t"].to_i, jd_score: entry["gsx$jd"]["$t"].to_f, hunter_score: entry["gsx$hunter"]["$t"].to_f, dave_score: entry["gsx$dave"]["$t"].to_f, steve_score: entry["gsx$steve"]["$t"].to_f, episode: episode)
   song.credits << credit
 end
