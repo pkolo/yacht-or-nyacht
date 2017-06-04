3c39a4483e05eb64124d07dc2fa18a761dc905c8
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-04 15:22:52 -0400

    Session routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 get '/login' do
   erb :'sessions/new'
 end
+
+post '/login' do
+  @user = User.find_by_username(params[:username]).try(:authenticate, params[:password])
+  if @user
+    login
+    redirect '/'
+  else
+    @errors = ["Email/password does not match"]
+    erb :'sessions/new'
+  end
+end
