14319706ec920dd83523249be4caaddaaa3752e7
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-05 14:49:15 -0400

    Front end search structure

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 get '/search' do
   @q = params[:q]
-  @song_results = song_match(q)
-  @personnel_results = personnel_match(q)
+  @song_results = song_match(@q)
+  @personnel_results = personnel_match(@q)
   erb :'search/show'
 end
