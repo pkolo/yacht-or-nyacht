d9c4dfc5a05d4f6965fef6af80e8538f7602a9f0
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-05 16:06:23 -0400

    Add slugs to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,14 @@ response = Net::HTTP.get_response(uri)
 json_res = JSON.parse(response.body)
 
 json_res["feed"]["entry"].each do |entry|
-  artist = Personnel.find_or_create_by(name: entry["gsx$artist"]["$t"])
+  artist = Personnel.find_or_create_by(name: entry["gsx$artist"]["$t"], slug: sluggify(entry["gsx$artist"]["$t"]))
   credit = Credit.new(personnel: artist, role: "Artist")
   episode = Episode.find_or_create_by(number: entry["gsx$show"]["$t"])
 
-  song = Song.create(title: entry["gsx$title"]["$t"], year: entry["gsx$year"]["$t"].to_i, jd_score: entry["gsx$jd"]["$t"].to_f, hunter_score: entry["gsx$hunter"]["$t"].to_f, dave_score: entry["gsx$dave"]["$t"].to_f, steve_score: entry["gsx$steve"]["$t"].to_f, episode: episode)
+  song = Song.create(title: entry["gsx$title"]["$t"], year: entry["gsx$year"]["$t"].to_i, jd_score: entry["gsx$jd"]["$t"].to_f, hunter_score: entry["gsx$hunter"]["$t"].to_f, dave_score: entry["gsx$dave"]["$t"].to_f, steve_score: entry["gsx$steve"]["$t"].to_f, episode: episode, slug: sluggify(entry["gsx$title"]["$t"]))
   song.credits << credit
 end
+
+u = User.new(name: ENV['ADMIN_NAME'])
+u.password = ENV['ADMIN']
+u.save
