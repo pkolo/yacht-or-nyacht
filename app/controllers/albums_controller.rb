b36b10f2e32c56816d95a00757ed83889d1db58c
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-06 11:54:06 -0400

    Add subtitle to pages

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,6 @@
 get '/albums/:slug' do
   @album = Album.find_by(slug: params[:slug])
+  @subtitle = @album.title
   @songs = @album.songs.sort_by { |song| song.yachtski }.reverse
   erb :'albums/show'
 end
