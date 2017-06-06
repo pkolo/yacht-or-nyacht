ba157b6373812f8ce80a44060db6197f1cdffedc
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-06 09:54:33 -0400

    Remove Artist mvc

--- app/controllers/artists_controller.rb
+++
@@ -1,5 +0,0 @@
-get '/artists/:id' do
-  @artist = Artist.find(params[:id])
-  @songs = @artist.songs
-  erb :'artists/show'
-end
--- app/models/artist.rb
+++
@@ -1,8 +0,0 @@
-class Artist < ActiveRecord::Base
-  has_many :songs
-  has_many :albums
-
-  def total_avg
-    ((self.songs.inject(0) {|sum, song| sum + song.yachtski}) / self.songs.length.to_f).round(2)
-  end
-end
