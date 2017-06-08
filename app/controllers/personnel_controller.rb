91f83d5524d0260f2e6ca92aef552e2f424e259e
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-08 10:17:42 -0400

    Personnel credits display in yachtski order

--- app/controllers/personnel_controller.rb
+++ app/controllers/personnel_controller.rb
@@ -3,7 +3,7 @@ get '/personnel/:slug' do
   @subtitle = @personnel.name
   @yachtski = @personnel.yachtski
   @song_performances = @personnel.song_performances.sort_by {|song| song.yachtski }.reverse
-  @song_credits = @personnel.combined_song_credits.sort_by { |credit| credit[:yachtski] }.reverse
-  @album_credits = @personnel.combined_album_credits.sort_by { |credit| credit[:yachtski] }.reverse
+  @song_credits = @personnel.combined_song_credits.sort_by { |credit| credit[:media].yachtski }.reverse
+  @album_credits = @personnel.combined_album_credits.sort_by { |credit| credit[:media].yachtski }.reverse
   erb :'personnel/show'
 end
