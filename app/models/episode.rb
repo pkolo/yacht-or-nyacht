5a9933d7f3062d1ce411168d80a0b5fb780e6260
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-08 10:51:37 -0400

    Title generator for episode

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,12 @@ class Episode < ActiveRecord::Base
 
   validates :number, uniqueness: true
 
+  def title
+    if self.number.match('YON')
+      self.number.gsub('YON', 'Yacht Or Nyacht #')
+    else
+      self.number.gsub('BYR', 'Beyond Yacht Rock #')
+    end
+  end
+
 end
