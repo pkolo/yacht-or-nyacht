341b52d43f54d90a6c62d6071f680c4de1d5cdad
Patrick Kolodgy <pkolodgy@gmail.com>
2017-07-19 09:24:42 -0400

    Fixes edit page

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -30,6 +30,7 @@ end
 get '/episodes/:id/edit' do
   if logged_in?
     @episode = Episode.find(params[:id])
+    @songs = @episode.songs.sort_by { |song| song.yachtski }.reverse
     erb :'episodes/edit'
   else
     redirect '/'
