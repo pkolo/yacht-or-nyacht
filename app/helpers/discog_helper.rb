53323aec1c827ac9c6b49ad0fcccf55a389b87ee
Patrick Kolodgy <pkolodgy@gmail.com>
2017-07-28 11:28:29 -0400

    Renames search

--- app/helpers/discog_helper.rb
+++ app/helpers/discog_helper.rb
@@ -1,31 +1,12 @@
 module DiscogHelper
 
-  def includes_track?(all_tracks, person, track_no)
-    extract_range = person["tracks"].split(", ").map do |track|
-      if track.include?("to")
-        range = track.split(" to ")
-        start = all_tracks.index(range[0])
-        fin = all_tracks.index(range[1])
-        all_tracks[start..fin]
-      elsif track.include?("-")
-        range = track.split("-")
-        start = all_tracks.index(range[0])
-        fin = all_tracks.index(range[1])
-        all_tracks[start..fin]
-      else
-        track
-      end
-    end
-    extract_range.flatten.include?(track_no)
-  end
-
   def api_call(url)
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
   end
 
-  def discog_search(options)
+  def search(options)
     q = self.build_query(options)
     url = "https://api.discogs.com/database/search?#{q}"
     response = api_call(url)
@@ -48,6 +29,25 @@ module DiscogHelper
     end
 
     q
+  end
+
+  def includes_track?(all_tracks, person, track_no)
+    extract_range = person["tracks"].split(", ").map do |track|
+      if track.include?("to")
+        range = track.split(" to ")
+        start = all_tracks.index(range[0])
+        fin = all_tracks.index(range[1])
+        all_tracks[start..fin]
+      elsif track.include?("-")
+        range = track.split("-")
+        start = all_tracks.index(range[0])
+        fin = all_tracks.index(range[1])
+        all_tracks[start..fin]
+      else
+        track
+      end
+    end
+    extract_range.flatten.include?(track_no)
   end
 
 end
