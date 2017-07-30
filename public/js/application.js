0e0568a85393a5b7bfee03fe4adc09374a8252a1
Patrick Kolodgy <pkolodgy@gmail.com>
2017-07-30 11:59:24 -0400

    Search form works async

--- public/js/application.js
+++ public/js/application.js
@@ -186,9 +186,26 @@ $(document).ready(function() {
     });
   });
 
-  $('#song-checker-form').submit(function(e) {
+  $('body').on('submit', '#song-checker-form', function(e) {
+    e.preventDefault();
     $('.loading').empty();
     $('.loading').append('<h3>Loading...</h3><p>(This might take a while)</p>');
+    $.ajax({
+      url: '/songs/search',
+      method: 'GET',
+      data: $(this).serialize()
+    }).done(function(res) {
+      $('.main-wrapper').empty();
+      $('.main-wrapper').append(res);
+      $('.yachtski-list-item').each(function() {
+        var $this = $(this);
+        var yachtski = $this.text();
+        var color = getColor(yachtski);
+
+        $this.siblings('.bullet').css('background-color', color);
+        $this.parent().css('border-bottom', `1px solid ${color}`);
+      });
+    });
   });
 
 // Responsive
