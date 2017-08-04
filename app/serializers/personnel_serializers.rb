45b9c5ecbd5f83b6d8265e8ba031c15fa571780d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-04 14:17:45 -0400

    Personnel serializes own credits list

--- app/serializers/personnel_serializers.rb
+++ app/serializers/personnel_serializers.rb
@@ -2,9 +2,12 @@ module PersonnelSerializers
   def serialize(args={})
     return self.link_serializer if args[:basic]
 
-    {
+    serialized_personnel = self.link_serializer
+    serialized_personnel[:song_performances] = self.song_performances.map {|song| song.serialize }
+    serialized_personnel[:song_credits] = self.serialize_song_credits
+    serialized_personnel[:album_credits] = self.serialize_album_credits
 
-    }
+    serialized_personnel
   end
 
   def link_serializer
@@ -14,6 +17,28 @@ module PersonnelSerializers
       resource_url: "/personnel/#{self.slug}",
       yachtski: self.yachtski
     }
+  end
+
+  def serialize_song_credits
+    combined_credits = self.song_credits.uniq.each_with_object([]) do |credit, memo|
+      combined_credit = {
+        media: credit.creditable.serialize,
+        roles: self.song_credits.where(creditable_id: credit.creditable.id).pluck(:role)
+      }
+      memo << combined_credit
+    end
+    combined_credits.uniq {|c| c[:media]}
+  end
+
+  def serialize_album_credits
+    combined_credits = self.album_credits.each_with_object([]) do |credit, memo|
+      combined_credit = {
+        media: credit.creditable.serialize,
+        roles: self.album_credits.where(creditable_id: credit.creditable.id).pluck(:role)
+      }
+      memo << combined_credit
+    end
+    combined_credits.uniq {|c| c[:media]}
   end
 
 end
