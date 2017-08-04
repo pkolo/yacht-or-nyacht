6932efbe8488a9a6504ee668de8b240c1c63b326
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-04 12:35:27 -0400

    Removes pry

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,7 @@
 get '/albums/:slug' do
   album = Album.find_by(slug: params[:slug]).serialize.to_json
   @album = JSON.parse(album)
-  binding.pry
+
   @subtitle = @album.title
   @songs = @album.songs.sort_by { |song| song.yachtski }.reverse
   erb :'albums/show'
