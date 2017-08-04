b41140ae66ab0cd8de3fd5160fbac069d8eafc16
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-04 12:57:05 -0400

    Gets extended album data

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,9 +1,8 @@
 get '/albums/:slug' do
-  album = Album.find_by(slug: params[:slug]).serialize.to_json
+  album = Album.find_by(slug: params[:slug]).serialize({extended: true}).to_json
   @album = JSON.parse(album)
   @subtitle = @album['title']
 
-
-  # @songs = @album['tracklist'].sort_by { |song| song['scores']['yachtski'] }.reverse
+  @songs = @album['tracklist'].sort_by { |song| song['scores']['yachtski'] }.reverse
   erb :'albums/show'
 end
