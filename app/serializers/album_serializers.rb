8ff40319dcaaf39dc88c8448302f0af9b04b546a
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-04 12:00:24 -0400

    Adds year to album serial

--- app/serializers/album_serializers.rb
+++ app/serializers/album_serializers.rb
@@ -3,6 +3,7 @@ module AlbumSerializers
   def serialize(args={})
     serialized_album = {
       title: self.title,
+      year: self.year,
       resource_url: "/albums/#{self.slug}",
       personnel: self.serialize_credits(self.credits.player_credits)
     }
