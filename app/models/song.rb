1b3fd25f5210a1d577111eea632131cc53c2c792
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-04 09:45:00 -0400

    Cleans up credit serializer output

--- app/models/song.rb
+++ app/models/song.rb
@@ -86,11 +86,9 @@ class Song < ActiveRecord::Base
   def serialize_credits(credits_list)
     # Combine players by name, combine their roles
     personnel = credits_list.uniq { |p| p[:personnel].id }.each_with_object([]) do |credit, memo|
-      combined_roles = {
-        person_data: credit.personnel.serializer({basic: true}),
-        roles: credits_list.where(personnel_id: credit.personnel.id).pluck(:role)
-      }
-      memo << combined_roles
+      person_data = credit.personnel.serializer({basic: true})
+      person_data[:roles] = credits_list.where(personnel_id: credit.personnel.id).pluck(:role)
+      memo << person_data
     end
   end
 
