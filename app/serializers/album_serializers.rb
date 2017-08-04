74535ab76ea379b7ea6837611b5cf8e56d76c0df
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-04 12:35:03 -0400

    Album serializer handles credits

--- app/serializers/album_serializers.rb
+++ app/serializers/album_serializers.rb
@@ -2,11 +2,19 @@ module AlbumSerializers
 
   def serialize(args={})
     serialized_album = {
+      artists: self.serialize_credits(self.credits.artist_credits),
       title: self.title,
       year: self.year,
       resource_url: "/albums/#{self.slug}",
       personnel: self.serialize_credits(self.credits.player_credits)
     }
+
+    if args[:extended]
+      serialized_album[:yachtski] = self.yachtski
+      serialized_album[:tracklist] = self.songs.map {|song| song.serialize}
+    end
+
+    serialized_album
   end
 
 end
