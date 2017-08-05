01f01d37cfa0287d58d789dc57b144e72818d0cb
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-05 09:26:23 -0400

    Updates personnel yachtski after credit creation

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,4 +1,10 @@
 class Credit < ActiveRecord::Base
   belongs_to :personnel
   belongs_to :creditable, polymorphic: true
+
+  after_create :update_personnel_yachtski
+
+  def update_personnel_yachtski
+    self.personnel.write_yachtski
+  end
 end
