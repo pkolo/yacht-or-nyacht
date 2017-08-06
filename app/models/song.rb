6e861b7be56558c9339390f205ec6d2178298e51
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-06 08:44:30 -0400

    Adds performers assoc for quick name search

--- app/models/song.rb
+++ app/models/song.rb
@@ -30,7 +30,7 @@ class Song < ActiveRecord::Base
   end
 
   # has_many :players, ->(credit) { where("credits.role NOT IN (?)", ["Artist", "Duet", "Featuring"]) }, through: :credits, source: :personnel
-  # has_many :performers, ->(credit) { where 'credits.role IN (?)', ["Artist"] }, through: :credits, source: :personnel
+  has_many :performers, ->(credit) { where 'credits.role IN (?)', ["Artist"] }, through: :credits, source: :personnel
   # has_many :features, ->(credit) { where 'credits.role IN (?)', ["Duet", "Featuring"] }, through: :credits, source: :personnel
 
   default_scope { order(yachtski: :desc) }
