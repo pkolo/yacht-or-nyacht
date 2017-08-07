978f7c06358cb38a45150fef727f86743554dabd
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-07 09:22:39 -0400

    Removes obsolete methods

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -12,9 +12,6 @@ class Personnel < ActiveRecord::Base
   has_many :song_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Song'
   has_many :album_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Album'
 
-  # has_many :song_credits, ->(credit) { where 'credits.role != ? AND credits.creditable_type = ?', "Artist", "Song" }, class_name: 'Credit'
-  # has_many :album_credits, ->(credit) { where 'credits.role != ? AND credits.creditable_type = ?', "Artist", "Album" }, class_name: 'Credit'
-
   default_scope { order(yachtski: :desc) }
 
   after_create :create_slug
