9bec07a94b71e80ee431a4670de1f5dafdfb72ef
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-07 09:46:15 -0400

    Adds performance scopes to credits

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,4 +2,7 @@ class Credit < ActiveRecord::Base
   belongs_to :personnel
   belongs_to :creditable, polymorphic: true
 
+  scope :artist, -> { where 'role IN (?)', ["Artist"] }
+  scope :feature, -> { where 'role IN (?)', ["Duet", "Featuring"] }
+  scope :player, -> { where 'role NOT IN (?)', ["Artist", "Duet", "Featuring"]}
 end
