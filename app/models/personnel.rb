4b2ffc012e82d98d9bd3e1c5f5b1c1f20e89cc16
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-07 09:00:10 -0400

    Adds distinct select to album and song assoc

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -6,8 +6,8 @@ class Personnel < ActiveRecord::Base
   include CreditableSerializers
 
   has_many :credits
-  has_many :songs, through: :credits, source: :creditable, source_type: 'Song'
-  has_many :albums, through: :credits, source: :creditable, source_type: 'Album'
+  has_many :songs, -> { distinct }, through: :credits, source: :creditable, source_type: 'Song'
+  has_many :albums, -> { distinct }, through: :credits, source: :creditable, source_type: 'Album'
 
   has_many :song_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Song'
   has_many :album_performances, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Album'
